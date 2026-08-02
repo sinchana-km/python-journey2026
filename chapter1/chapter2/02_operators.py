9eed6c69c6dce9c4d7e3cb159239383973943d81
sinchana <sinchana10120@gmail.com>
2026-08-02 19:53:33 +0530

    updated files

---
+++ chapter1/chapter2/02_operators.py
@@ -0,0 +1,39 @@
+a=7
+b=3
+#arithmetic operators in Python
+print()
+print("a + b = ",a + b)
+print("a - b = ",a - b)
+print("a * b = ",a * b)
+print("a / b = ",a / b)
+
+#Assignment operator
+print("Demonstrating Assignment Operators in Python")
+c = 5
+d = 7
+
+print(c, d)
+c += 5
+d += 1
+print(c, d) 
+
+#comparision operator
+print("Demonstrating Comparison Operators in Python")
+e = 6
+f = 9
+print(e==f) #it return false becouse 6 not equal to 9
+print(e>f) 
+print(e<f) 
+print(e!=f) 
+
+
+
+#Logical operator
+print("Demonstrating Logical Operators in Python")
+e = 6
+f = 9
+print(e==f and e>f) 
+print(e==f or e<f) 
+print(not(e>f)) 
+
+
