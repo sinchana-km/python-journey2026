92b9cb6fd34892458b5c96dd82d5912ab03e8bd4
sinchana-km <sinchana1012003@gmail.com>
2026-08-13 18:42:51 +0530

    project2 is added

---
+++ chapter1/chapter2/chapter3/chapter5/chapter6/chapter7/chapter8/projects/chapter9/project2.py
@@ -0,0 +1,17 @@
+import random
+
+number = random.randint(1, 100)
+guess = int(input("Guess the number: "))
+
+attemts = 1
+while(True):
+    
+ if(guess>number):
+    guess = int(input("Guess another number. this one is too big"))
+    attemts +=1
+ elif(guess<number):
+    guess = int(input("Guess another number,this one is too less"))
+    attemts +=1
+ else:
+    print(f"yeah thats the number! you guessed it right in {attemts} attems")
+    break
